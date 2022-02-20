b176468492fe3431efdd4eed37357eb14859738b
Christian Gregersen <chgr007@student.kristiania.no>
2022-02-20 15:57:02 +0100

    "Tests for answers" 🟢

--- server/quizApp.js
+++ server/quizApp.js
@@ -4,28 +4,28 @@ import { Questions, randomQuestion, isCorrectAnswer } from "./questions.js";
 export const QuizApp = express.Router();
 
 QuizApp.get("/question", (req, res) => {
-    let { question, answers, id } = randomQuestion();
-    answers = Object.keys(answers)
-        .filter(a => answers[a] !== null)
-        .reduce((obj, key) => {
-            obj[key] = answers[key];
-            return obj;
-        }, {});
+  let { question, answers, id } = randomQuestion();
+  answers = Object.keys(answers)
+    .filter((a) => answers[a] !== null)
+    .reduce((obj, key) => {
+      obj[key] = answers[key];
+      return obj;
+    }, {});
 
-    res.json({question,answers,id});
+  res.json({ question, answers, id });
 });
 
 QuizApp.post("/answer", (req, res) => {
-    const {id, answer} = req.body;
-    const question = Questions.find((q) => q.id === id);
+  const { id, answer } = req.body;
+  const question = Questions.find((q) => q.id === id);
 
-    if(!question){
-        return res.sendStatus(404);
-    }
+  if (!question) {
+    return res.sendStatus(404);
+  }
 
-    if(isCorrectAnswer(question, answer)){
-        return res.json({result: "correct"});
-    } else {
-        return res.json({result: "incorrect"});
-    }
+  if (isCorrectAnswer(question, answer)) {
+    return res.json({ result: "correct" });
+  } else {
+    return res.json({ result: "incorrect" });
+  }
 });
