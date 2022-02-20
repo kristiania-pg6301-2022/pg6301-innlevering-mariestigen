d540f6a3f91df051098a67aa39e3b14621f9e5fe
Christian Gregersen <chgr007@student.kristiania.no>
2022-02-20 19:01:11 +0100

    added scaffolding for frontend tests

---
+++ __tests__/clientTest.js
@@ -0,0 +1,9 @@
+describe("Tests for the front end", () => {
+  it("Should render a question", () => {});
+
+  it("Should render answer options", () => {});
+
+  it("Should render correct answer", () => {});
+
+  it("Should render wrong answer", () => {});
+});
