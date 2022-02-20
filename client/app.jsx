c48c1c0145830008a854779bdab52a5350f75206
mariestigen <marie.stigen@hotmail.com>
2022-02-20 17:23:48 +0100

    mini-setup of frontend handeling

---
+++ client/app.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import {fetchJSON, postJSON} from "http.js";
+import {useLoader} from "useLoader.js";
+
+function showQuestion({question, onReload}){
+
+}
+
+function QuestionComponent({reload}){
+
+}
+
+export function App(){
+
+}
