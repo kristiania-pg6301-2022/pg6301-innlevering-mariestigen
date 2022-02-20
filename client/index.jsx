f338fe0eca6a88388f01948dbece1e5cfed9a025
Christian Gregersen <chgr007@student.kristiania.no>
2022-02-20 17:30:57 +0100

    added "test:coverage" script for flexibility. Changed husky command to run & updated the "verify" script to reflect above-mentioned changes

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {App} from "app.jsx"
+import { App } from "app.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// Trigger change to commit!
