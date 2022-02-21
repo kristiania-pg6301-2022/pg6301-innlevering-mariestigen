20b82629ba3ff953dfc13bc03b5d1f6fb180221f
Christian Gregersen <chgr007@student.kristiania.no>
2022-02-21 21:51:10 +0100

    client test go brrrrr 🔴🔴🔴🔴🔴🔴🔴

--- client/__tests__/client.test.jsx
+++ client/__tests__/client.test.jsx
@@ -1,8 +1,60 @@
-import { App } from "../app.jsx";
-import { jest } from "jest";
+import { App, QuestionComponent, ShowQuestion } from "../app.jsx";
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act, Simulate } from "react-dom/test-utils";
 
-describe("aa", () => {
-  it("aa", () => {
-    expect(true);
+let container = null;
+const fakeQuestion = {
+  id: 974,
+  question:
+    "What is the correct JavaScript syntax to change the content of the HTML element below?",
+  answers: {
+    answer_a: '#demo.innerHTML = "Hello World!";',
+    answer_b: 'document.getElementById("demo").innerHTML = "Hello World!";',
+    answer_c: 'document.getElement("p").innerHTML = "Hello World!";',
+    answer_d: 'document.getElementByName("p").innerHTML = "Hello World!";',
+  },
+};
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+function setupFetchStub(data) {
+  return function fetchStub(_url) {
+    return new Promise((resolve) => {
+      resolve({
+        json: () =>
+          Promise.resolve({
+            data,
+          }),
+      });
+    });
+  };
+}
+describe("Tests for client", () => {
+  it("Should render a question", async () => {
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementation(setupFetchStub(fakeQuestion));
+
+    await act(async () => {
+      //const setLoadQuestion = jest.fn();
+
+      render(<App />, container);
+    });
+
+    Simulate.click(container.querySelector("#load-question-btn"));
+    expect(container.querySelector("#question-container h2").textContent).toBe(
+      fakeQuestion.question
+    );
+
+    global.fetch.mockRestore();
   });
 });
--- client/app.jsx
+++ client/app.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { fetchJSON, postJSON } from "./http.jsx";
 import { useLoader } from "./useLoader.jsx";
 
-function ShowQuestion({ question, onReload }) {
+export function ShowQuestion({ question, onReload }) {
   async function handleAnswer(answer) {
     const { id } = question;
     postJSON("/api/answer", { id, answer });
@@ -10,7 +10,7 @@ function ShowQuestion({ question, onReload }) {
   }
 
   return (
-    <div>
+    <div id={"question-container"}>
       <h2>{question.question}</h2>
       {Object.keys(question.answers).map((a) => (
         <div key={a}>
@@ -21,7 +21,7 @@ function ShowQuestion({ question, onReload }) {
   );
 }
 
-function QuestionComponent({ reload }) {
+export function QuestionComponent({ reload }) {
   const [question, setQuestion] = useState();
 
   async function handleLoadQuestion() {
@@ -37,7 +37,9 @@ function QuestionComponent({ reload }) {
   if (!question) {
     return (
       <div>
-        <button onClick={handleLoadQuestion}>Load a new question</button>
+        <button id="load-question-btn" onClick={handleLoadQuestion}>
+          Load a new question
+        </button>
       </div>
     );
   }
